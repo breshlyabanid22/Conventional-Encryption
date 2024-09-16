b0d99dc96f93ceddc46afabe0f559f3bb6c0b7c6
Breshly Abanid <breshlyabanid@gmail.com>
2024-09-16 19:22:29 +0800

    Add project files.

---
+++ Activity1_ABANID/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Activity1_ABANID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog3 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog4 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Decrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 70);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Encrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Ceasar Cipher";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 222);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Shift Cipher";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(142, 294);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 59);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Encrypt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(436, 294);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 59);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Decrypt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog3
+            // 
+            this.openFileDialog3.FileName = "openFileDialog3";
+            this.openFileDialog3.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog3_FileOk);
+            // 
+            // openFileDialog4
+            // 
+            this.openFileDialog4.FileName = "openFileDialog4";
+            this.openFileDialog4.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog4_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog4;
+    }
+}
+
---
+++ Activity1_ABANID/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Activity1_ABANID
+{
+    public partial class Form1 : Form
+    {
+        private static readonly int Shift = 3;
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.OpenOrCreate);
+            int[] temp = new int[fs.Length];
+            for(int x = 0; x < fs.Length; x++)
+            {
+                temp[x] = fs.ReadByte();
+            }
+            fs.Close();
+            for(int x = 0; x < temp.Length; x++)
+            {
+                temp[x] += 3;
+            }
+            fs = new FileStream(openFileDialog1.FileName, FileMode.OpenOrCreate);
+            for(int x = 0; x < fs.Length; x++)
+            {
+                fs.WriteByte((byte)(temp[x]));
+            }
+            fs.Close();
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.OpenOrCreate);
+            int[] temp = new int[fs.Length];
+            for (int x = 0; x < fs.Length; x++)
+            {
+                temp[x] = fs.ReadByte();
+            }
+            fs.Close();
+            for (int x = 0; x < temp.Length; x++)
+            {
+                temp[x] -= 3;
+            }
+            fs = new FileStream(openFileDialog1.FileName, FileMode.OpenOrCreate);
+            for (int x = 0; x < fs.Length; x++)
+            {
+                fs.WriteByte((byte)(temp[x]));
+            }
+            fs.Close();
+            MessageBox.Show("File decrypted using Ceasar cipher!");
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog3.ShowDialog();
+        }
+
+        //Shift Cipher
+        private void EncryptFileSubstitution(string filePath, int shift)
+        {
+            byte[] fileContent = File.ReadAllBytes(filePath);
+            byte[] encryptedContent = SubstitutionEncryptDecrypt(fileContent, shift);
+            File.WriteAllBytes(filePath, encryptedContent);
+        }
+        private void DecryptFileSubstitution(string filePath, int shift)
+        {
+            byte[] encryptedContent = File.ReadAllBytes(filePath);
+            byte[] decryptedContent = SubstitutionEncryptDecrypt(encryptedContent, -shift);
+            File.WriteAllBytes(filePath, decryptedContent);
+        }
+
+        private byte[] SubstitutionEncryptDecrypt(byte[] data, int shift)
+        {
+            byte[] result = new byte[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                result[i] = (byte)(data[i] + shift);
+            }
+            return result;
+        }
+        private void openFileDialog3_FileOk(object sender, CancelEventArgs e)
+        {
+
+            string filePath = openFileDialog3.FileName;
+            EncryptFileSubstitution(filePath, Shift);
+            MessageBox.Show("File encrypted using Shift cipher!");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)  
+        {
+            openFileDialog4.ShowDialog();
+        }
+
+        private void openFileDialog4_FileOk(object sender, CancelEventArgs e)
+        {
+            string filePath = openFileDialog4.FileName;
+            DecryptFileSubstitution(filePath, Shift);
+            MessageBox.Show("File decrypted using Shift cipher!");
+
+        }
+
+    }
+
+}
